55622e13a9fa609401fc53e0b832d45372196ecb
jedrzej-gorski <jedrzej.gorski@student.put.poznan.pl>
2023-05-31 19:31:03 +0200

    Sequential implementation

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/users/gorsk/onedrive/documents/buda/[semVI]PR
+# Build directory: /mnt/c/users/gorsk/onedrive/documents/buda/[semVI]PR/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
